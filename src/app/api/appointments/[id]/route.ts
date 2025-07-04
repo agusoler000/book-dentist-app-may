7f2a8cce267da6a16b398b724f7b835ffd8815f2
Agustin Soler Conde <agustin.soler@cuatrodigital.com>
2025-07-04 09:18:07 +0200

    fix route appointment

--- src/app/api/appointments/[id]/route.ts
+++ src/app/api/appointments/[id]/route.ts
@@ -1,9 +1,10 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
+  const { params } = context;
   const session = await getServerSession(authOptions);
   if (!session || (session.user as any).role !== "DENTIST") {
     return NextResponse.json({ success: false, error: "Unauthorized" }, { status: 401 });
